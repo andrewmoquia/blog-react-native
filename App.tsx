9b8f68696c3c17fb15b640d9d06a5ae1946552ef
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-28 16:56:00 +0800

    feat: Add custom fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/Home.screen';
+import { useFonts } from 'expo-font';
 
 const App = () => {
+    const [fontsLoaded] = useFonts({
+        'Poppins-Thin': require('./assets/fonts/Poppins-Thin.ttf'),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='Search'>
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,11 +1,18 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <Text style={styles.textStyle}>Home</Text>
+            <Text>Home with no Style</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontFamily: 'Poppins-Thin',
+    },
+});
 
 export default HomeScreen;
