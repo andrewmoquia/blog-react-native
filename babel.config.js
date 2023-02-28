c48217b17577461109e155618cce91600a00cbf8
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-28 16:12:39 +0800

    feat: setup eslint and prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@components': './src/components',
+          },
+        },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP',
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
   };
 };
